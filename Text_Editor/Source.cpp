eb0000b58f2264bb059c308eae89a6f71713e26d
Bryan Rivas <bryan.rivas05@gmail.com>
2024-11-22 17:53:46 -0500

    Rendered a simple FLTK window

--- Text_Editor/Source.cpp
+++ Text_Editor/Source.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <FL/Fl_Double_Window.H>
+#include <FL/Fl.H>
+#include <FL/Fl_Menu_Bar.H>
+#include <FL/fl_ask.H>
+#include <FL/filename.H>
+#include<FL/fl_string_functions.h>
+
+#include <Windows.h>
+#include <gdiplus.h>
+#include <objidl.h>
+#pragma comment(lib,"gdiplus.lib")
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "comctl32.lib")
 
 
-void Log(const char* message)
+Fl_Double_Window* app_window = NULL;
+
+
+void test_window()
 {
-	std::cout << message << std::endl;
+	app_window = new Fl_Double_Window(640, 480, "Title");
+}
+
+
+int main()
+{
+	test_window();
+	app_window->show();
+	return Fl::run();
 }
